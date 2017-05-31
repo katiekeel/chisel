de2f454dfef2b7d66014a6b32f6ed5454b27ef52
Katie Shermer <katieshermer@gmail.com>
2017-05-31 13:48:56 -0600

    Expanded conversion_key hash and modified convert_entire_string_to_html to reflect; added more testing

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -8,42 +8,23 @@ class Chisel
 
   def conversion_key
     conversion_key = {
-      "#" => "<h1>",
-      "##" => "<h2>",
-      "###" => "<h3>",
+      "##### " => "<h5>",
+      "#### " => "<h4>",
+      "### " => "<h3>",
+      "## " => "<h2>",
+      "# " => "<h1>",
+      " #" => "</h1>",
       "\n\n" => "<p>"
     }
   end
 
-  def convert
-    convert_single_hashtag_to_h1
-    convert_double_hashtag_to_h2
-  end
-
-  def convert_single_hashtag_to_h1
-    if @input.include?("#")
-      output = @input.gsub("#", "<h1>")
-    end
-    output
-  end
-
-  def convert_double_hashtag_to_h2
-    if @input.include?("##")
-      output = @input.gsub("##", "<h2>")
-    end
-    output
-  end
-
   def convert_entire_string_to_html
-    output_string = ""
-    @input.each_char do |char|
-      if conversion_key.key?(char)
-        output_string += conversion_key[char]
-      else
-        output_string += char
+    conversion_key.each do |key, value|
+      if input.include?(key)
+        @input.gsub!(key, value)
       end
     end
-    output_string
+    @input
   end
 
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -17,33 +17,28 @@ class ChiselTest < Minitest::Test
   end
 
   def test_convert_single_hashtag_to_h1
-    chisel = Chisel.new("#")
-    assert_equal chisel.convert_single_hashtag_to_h1, "<h1>"
-  end
-
-  def test_convert_double_hashtag_to_h2
-    chisel = Chisel.new("##")
-    assert_equal chisel.convert_double_hashtag_to_h2, "<h2>"
-  end
-
-  def test_convert_entire_string_to_html_with_hashtag
-    chisel = Chisel.new("#")
+    chisel = Chisel.new("# ")
     assert_equal chisel.convert_entire_string_to_html, "<h1>"
   end
 
-  def test_convert_entire_string_to_html_with_hashtag_and_two_chars
-    chisel = Chisel.new("#A")
-    assert_equal chisel.convert_entire_string_to_html, "<h1>A"
+  def test_convert_double_hashtag_to_h2
+    chisel = Chisel.new("## ")
+    assert_equal chisel.convert_entire_string_to_html, "<h2>"
   end
 
-  def test_convert_entire_string_to_html_with_hashtag_and_two_words
-    chisel = Chisel.new("#A bug#")
-    assert_equal chisel.convert_entire_string_to_html, "<h1>A bug<h1>"
+  def test_convert_triple_hashtag_to_h3
+    chisel = Chisel.new("### ")
+    assert_equal chisel.convert_entire_string_to_html, "<h3>"
   end
 
-  def test_convert_method_with_double_hashtag
-    chisel = Chisel.new("##")
-    assert_equal chisel.convert, "<h2>"
+  def test_convert_quad_hashtag_to_h4
+    chisel = Chisel.new("#### ")
+    assert_equal chisel.convert_entire_string_to_html, "<h4>"
+  end
+
+  def test_convert_penta_hashtag_to_h5
+    chisel = Chisel.new("##### ")
+    assert_equal chisel.convert_entire_string_to_html, "<h5>"
   end
 
 end
