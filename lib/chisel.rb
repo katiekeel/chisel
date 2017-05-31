2a1a05bcd6ca233ad86eec2412a1be38e349dab0
Katie Shermer <katieshermer@gmail.com>
2017-05-31 13:25:09 -0600

    Added convert_entire_string_to_html method and tests

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -8,7 +8,8 @@ class Chisel
 
   def conversion_key
     conversion_key = {
-
+      "#" => "<h1>",
+      "##" => "<h2>"
     }
   end
 
@@ -16,6 +17,18 @@ class Chisel
     if @input.include?("#")
       @input.gsub!("#", "<h1>")
     end
+  end
+
+  def convert_entire_string_to_html
+    output_string = ""
+    @input.each_char do |char|
+      if conversion_key.key?(char)
+        output_string += conversion_key[char]
+      else
+        output_string += char
+      end
+    end
+    output_string
   end
 
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -19,6 +19,22 @@ class ChiselTest < Minitest::Test
   def test_convert_single_hashtag_to_markdown
     chisel = Chisel.new("#")
     assert_equal chisel.convert_single_hashtag_to_h1, "<h1>"
-  end    
+  end
+
+  def test_convert_entire_string_to_html_with_hashtag
+    chisel = Chisel.new("#")
+    assert_equal chisel.convert_entire_string_to_html, "<h1>"
+  end
+
+  def test_convert_entire_string_to_html_with_hashtag_and_two_chars
+    chisel = Chisel.new("#A")
+    assert_equal chisel.convert_entire_string_to_html, "<h1>A"
+  end
+
+  def test_convert_entire_string_to_html_with_hashtag_and_two_words
+    chisel = Chisel.new("#A bug#")
+    assert_equal chisel.convert_entire_string_to_html, "<h1>A bug<h1>"
+  end
+
 
 end
